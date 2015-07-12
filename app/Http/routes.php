b4f02b9ce1342b9543a3b0f9d903ffe84c283eca
Joel Davenport <j1edward1@yahoo.com>
2015-07-11 18:49:22 -0700

    Configure registration and authentication routes.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,6 +19,14 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
 
 Route::resource('feed', 'FeedController',
                     array('only' => array('index')));
